e76922ba3f7ff04f3b68b1d1c4bfbf8b9d7c84a2
t-robarge <timothy.robarge@uconn.edu>
2023-05-06 22:37:23 -0400

    removed color class

--- simulator_gui.py
+++ simulator_gui.py
@@ -7,19 +7,6 @@ from are_you_the_one import Contestant, Simulation
 from histogram import random_histogram, naive_histogram, optimized_histogram, comparison_histogram
 s1 = Simulation()
 
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
-#print(color.BOLD + 'Hello, World!' + color.END)
 class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
